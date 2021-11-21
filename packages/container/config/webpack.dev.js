298c034102d4c862e764908d3e73b39bef2fe7f8
Diego Marcuz <diego.marcuz@hotmail.com>
2021-11-21 18:24:02 -0300

    create container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,8 @@
 const {merge} = require("webpack-merge")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 const commonConfig = require('./webpack.common')
+const packageJson = require("../package.json")
 
 const devConfig = {
     mode:"development",
@@ -12,14 +13,13 @@ const devConfig = {
         }
     },
     plugins:[
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
+        
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            }
+            },
+            shared: packageJson.dependencies
         })
     ]
 }
