699ab6b0eee17d6b10c6574284a172aa95847c27
Diego Marcuz <diego.marcuz@hotmail.com>
2022-03-07 09:17:04 -0300

    container 3

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import {MarketingApp}  from './components/MarketingApp'
 export default () =>{
     return (
         <>
-        <h1>hi there Container 2</h1>
+        <h1>hi there Container 3</h1>
         <hr/>
         <MarketingApp/>
         </>
